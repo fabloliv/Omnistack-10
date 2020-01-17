06b4bb5507e4e23e7b5bcc6cfed2ea5394265442
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-01-17 06:20:57 -0300

    Make Express understand JSON

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+/*
+ * Query Params: request.query ( Filtros, ordenação, paginação,...)
+ * Route Params: request.params ( Identificar um recurso na alteração ou remoção )
+ * Body: request.body ( Dados para criação ou alteração de um registro )
+ */
+
 // Criando rotas
 app.get("/", (request, response) => {
   return response.json({ message: "hello omnistack week" });
