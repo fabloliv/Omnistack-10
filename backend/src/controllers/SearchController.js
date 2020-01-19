3a77677aad4644098e73fec13f2420f23d3d3333
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-01-19 08:07:42 -0300

    Test query search

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -2,6 +2,11 @@ const Dev = require("../models/Dev");
 
 module.exports = {
   async index(request, response) {
-    // busca
+    // busca por geolocalização
+    // filtrar por techs
+
+    console.log(request.query);
+
+    return response.json({ devs: [] });
   }
 };
