2021fbcbdcfb513d35a621fb1da27d6513548fd5
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-01-19 08:39:09 -0300

    Finish search for techs and location

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -3,15 +3,28 @@ const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
   async index(request, response) {
-    // busca por geolocalização
+    // busca por geolocalização 10km
     // filtrar por techs
 
     const { latitude, longitude, techs } = request.query;
 
     const techsArray = parseStringAsArray(techs);
 
-    console.log(techsArray);
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
+      }
+    });
 
-    return response.json({ devs: [] });
+    return response.json({ devs });
   }
 };
