09ecc6952e8e9e05553926aa2bfdad5588199ba9
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-01-19 07:40:21 -0300

    Avoid duplication of records

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,28 +5,32 @@ module.exports = {
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    // separa onde encontrar vírgula e remove espaços antes e depois
-    const techsArray = techs.split(",").map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      // separa onde encontrar vírgula e remove espaços antes e depois
+      const techsArray = techs.split(",").map(tech => tech.trim());
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return response.json(dev);
   }
